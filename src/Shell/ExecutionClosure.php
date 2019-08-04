9eaef06cca2a933d935951607ad060d403c4b6f2
Roberto Luna Rojas <roberto.luna.rojas@gmail.com>
2019-08-04 18:29:59 -0400

    Apply fixes from StyleCI (#3)

--- src/Shell/ExecutionClosure.php
+++ src/Shell/ExecutionClosure.php
@@ -28,7 +28,7 @@ class ExecutionClosure extends BaseExecutionClosure
                     \set_error_handler([$__psysh__, 'handleError']);
 
                     // Evaluate the current code buffer
-                    $_ = eval($__psysh__->onExecute($__psysh__->flushCode() ?: ExecutionClosure::NOOP_INPUT));
+                    $_ = eval($__psysh__->onExecute($__psysh__->flushCode() ?: self::NOOP_INPUT));
                 } catch (\Throwable $_e) {
                     // Clean up on our way out.
                     \restore_error_handler();
